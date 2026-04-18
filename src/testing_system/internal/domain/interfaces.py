ab97fc24c9a15e68b37a4a5865c42effb541d000
ne-F1anders <sosnovgreg@gmail.com>
2026-04-18 14:36:51 +0000

    feat: create ILogger (issue #25) to use in Runner

--- src/testing_system/internal/domain/interfaces.py
+++ src/testing_system/internal/domain/interfaces.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
+from typing import List
 from testing_system.internal.domain.value_objects import \
     AssistantRequest, AssistantResponse, \
     RetrievalResponse, RetrievalRequest
+from testing_system.internal.domain.entities import Experiment
 
 
 class IAssistant(ABC):
@@ -18,4 +20,29 @@ class IRetriever(ABC):
     @abstractmethod
     def retrieve(self, request: RetrievalRequest) -> RetrievalResponse:
         """Sends request to db and returns response with docs"""
+        pass
+
+class ILogger(ABC):
+    """Interface for versioning the experiments"""
+
+    @abstractmethod
+    def push(self, experiment: Experiment): #nothing to return or need smth?
+        """Saves the experiment into registry"""
+        pass
+
+    @abstractmethod
+    def check(self, experiment: Experiment) -> bool:
+        """
+        Checks the originality of the experiment`s configuration.
+        True - exists, don`t repeat
+        False - doesn`t exist, provide it and save
+        """
+        pass
+    
+    @abstractmethod
+    def select(self, latest = None) -> List[Experiment]:
+        """
+        Export all the experiments from registry.
+        latest: int for how many experiments to select, None for all
+        """
         pass
